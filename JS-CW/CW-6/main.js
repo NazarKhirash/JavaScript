04114d9bd2fd2eb654c397101bc97c1eada0b407
NazarKhirash <hirashnazar@ukt.net>
2021-12-04 13:03:07 +0200

    CW-6 done

--- JS-CW/CW-6/main.js
+++ JS-CW/CW-6/main.js
@@ -1,34 +1,61 @@
 // - Дано список імен.
-    let n1 = 'Harry..Potter'
-let n2 = 'Ron---Whisley'
-let n3 = 'Hermione__Granger'
+//     let n1 = 'Harry..Potter'
+// let n2 = 'Ron---Whisley'
+// let n3 = 'Hermione__Granger'
 // Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
 // let n1 = 'Harry Potter'
 // let n2 = 'Ron Whisley'
 // let n3 = 'Hermione Granger'
 
-let strTrim = (str)=>str.trim()
-console.log(strTrim(n2))
+// let strTrim = (str)=>str.replace('..',' ').replace('---',' ').replace('__',' ')
+// console.log(strTrim(n2))
 //
-//
+//----------------------------------------------------------------------------------------------------------------------
 // - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
-let arr = []
-let arrRandom = function (size,arr){
-    for (let i = 0; i < size; i++) {
-    return  Math.round(Math.random()*100)
-    }
-}
-console.log(arrRandom(arr))
-
+// let array = []
+// let arrRandom = function (size){
+//     for (let i = 0; i < size; i++) {
+//    array.push((Math.round(Math.random()*100)))
+//     }
+//     return array;
+// }
+// console.log(arrRandom(6))
+//----------------------------------------------------------------------------------------------------------------------
 
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+// let array = []
+// let arrRandom = function (size){
+//     for (let i = 0; i < size; i++) {
+//    array.push((Math.round(Math.random()*100)))
+//     }
+//     return array.sort();
+// }
+// console.log(arrRandom(6))
+//----------------------------------------------------------------------------------------------------------------------
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа
+// let array = []
+// let arrRandom = function (size){
+//     for (let i = 0; i < size; i++) {
+//    array.push((Math.round(Math.random()*100)))
+//     }
+//     return array.filter(value => value%2===0)
+// }
+// console.log(arrRandom(6))
+//----------------------------------------------------------------------------------------------------------------------
 // - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 //     let nums = [11,21,3];
 // sortNums('ascending') // [3,11,21]
 // sortNums('descending') // [21,11,3]
+
+// let nums = [11,21,3];
 //
+// let arrSortAscending = (arr)=>arr.sort((a,b)=>a-b)
+// console.log(arrSortAscending(nums))
+//
+// let arrSortDescending = (arr)=>arr.sort((a,b)=>b-a);
+// console.log(arrSortDescending(nums))
+//----------------------------------------------------------------------------------------------------------------------
 //
 // - є масив
 // let coursesAndDurationArray = [
@@ -41,3 +68,14 @@ console.log(arrRandom(arr))
 // ];
 // -- відсортувати його за спаданням за monthDuration
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// let arrObjSortMonth = (arr)=> {
+//    return  arr.sort((a,b)=>b.monthDuration-a.monthDuration).filter((arrElement)=>{
+//        if (arrElement.monthDuration>5){
+//            return arrElement
+//        }
+//    })
+// }
+// console.log(arrObjSortMonth(coursesAndDurationArray))
+
+
+
