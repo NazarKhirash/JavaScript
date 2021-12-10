5a72a95a9435d2e26475359bfc552477da5c7fbc
NazarKhirash <hirashnazar@ukt.net>
2021-12-10 12:46:19 +0200

    HW-8 done

---
+++ JS-HW/HW-8/main.js
@@ -0,0 +1,48 @@
+// Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+//
+// - Напишіть код,  котрий :
+// -- отримує текст з параграфа з id "content"
+
+// let pContent =document.getElementById('content')
+// console.log(pContent.innerText)
+//----------------------------------------------------------------------------------------------------------------------
+// -- отримує текст з блоку з id "rules"
+
+// let txtRules =document.getElementById('rules')
+// console.log(txtRules.innerText)
+//----------------------------------------------------------------------------------------------------------------------
+// -- замініть текст параграфа з id 'content' на будь-який інший
+
+// let changeTxtContent = document.getElementById('content')
+// changeTxtContent.innerText = 'hello'
+
+//----------------------------------------------------------------------------------------------------------------------
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+
+// let changeTxtRules = document.getElementById('rules')
+// changeTxtRules.innerText = 'world'
+//----------------------------------------------------------------------------------------------------------------------
+// -- змініть кожному елементу колір фону на червоний
+
+// changeTxtContent.style.backgroundColor ='blue'
+// changeTxtRules.style.backgroundColor = 'yellow'
+//----------------------------------------------------------------------------------------------------------------------
+// -- змініть кожному елементу колір тексту на синій
+
+// changeTxtContent.style.color = 'yellow'
+// changeTxtRules.style.color = 'blue'
+//----------------------------------------------------------------------------------------------------------------------
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// let getClassRules = changeTxtRules.classList;
+// for (const classRule of getClassRules) {
+//     console.log(classRule)
+// }
+
+//----------------------------------------------------------------------------------------------------------------------
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let colorRedFcRules = document.getElementsByClassName('fc_rules')
+// for (const colorRedFcRule of colorRedFcRules) {
+//     colorRedFcRule.style.color = 'red'
+// }
+//
