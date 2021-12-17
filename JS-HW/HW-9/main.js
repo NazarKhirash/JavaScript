9b22e5282d67643406048a95765f68e099f252e0
NazarKhirash <hirashnazar@ukt.net>
2021-12-17 21:00:49 +0200

    HW-9 done

--- JS-HW/HW-9/main.js
+++ JS-HW/HW-9/main.js
@@ -1,33 +1,33 @@
-// Все робити за допомоги js.
-// - створити блок,
+// // Все робити за допомоги js.
+// // - створити блок,
 // let newDiv = document.createElement('div')
-
-//     - додати йому класи wrap, collapse, alpha, beta
-// newDiv.classList.add('wrap','collapse', 'alpha', 'beta')
+//
+// // - додати йому класи wrap, collapse, alpha, beta
+// newDiv.classList.add('wrap', 'collapse', 'alpha', 'beta')
 // console.log(newDiv);
-
-// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+//
+// // - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
 // newDiv.style.height = '200px'
 // newDiv.style.width = '200px'
 // newDiv.style.backgroundColor = 'blue'
 // newDiv.innerHTML = 'hello'
 // newDiv.style.fontSize = '80px'
-
-// - додати цей блок в body.
-// document.body.append(newDiv)
-
-// - клонувати його повністю, та додати клон в body.
-// let cloneNewDiv = newDiv.cloneNode()
-// document.body.append(cloneNewDiv)
-
 //
-// - Є масив:
-//     ['Main','Products','About us','Contacts']
-// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
-// Завдання робити через цикли.
+// // - додати цей блок в body.
+// document.body.append(newDiv)
+//
+// // - клонувати його повністю, та додати клон в body.
+// let cloneNewDiv = newDiv.cloneNode()
+// // document.body.append(cloneNewDiv)
+//
+//
+// // - Є масив:
+// //     ['Main','Products','About us','Contacts']
+// // Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+// // Завдання робити через цикли.
 // let mainUl = document.getElementsByClassName('menu')[0]
 //
-// let arr = ['Main','Products','About us','Contacts']
+// let arr = ['Main', 'Products', 'About us', 'Contacts']
 // for (const item of arr) {
 //     let li = document.createElement('li')
 //
@@ -35,8 +35,8 @@
 //     li.append(item)
 // }
 //
-// -------------------------------------------------------------------------------------------------------------------------//
-// - Є масив
+// // -------------------------------------------------------------------------------------------------------------------------//
+// // - Є масив
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
 //     {title: 'Java Complex', monthDuration: 6},
@@ -58,9 +58,9 @@
 // }
 // document.body.append(mainDiv)
 //
-// ----------------------------------------------------------------------------------------------------------------------//
-// - Є масив
-// let coursesAndDurationArray = [
+// // ----------------------------------------------------------------------------------------------------------------------//
+// // - Є масив
+// let coursesAndDurationArray1 = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
 //     {title: 'Java Complex', monthDuration: 6},
 //     {title: 'Python Complex', monthDuration: 6},
@@ -71,10 +71,10 @@
 // // За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
 // //     Завдання робити через цикли.
 //
-// let mainDiv = document.createElement('div')
+// // let mainDiv = document.createElement('div')
 // mainDiv.classList.add('main')
 //
-// for (const value of coursesAndDurationArray) {
+// for (const value of coursesAndDurationArray1) {
 //     let itemDiv = document.createElement('div')
 //     itemDiv.classList.add('item')
 //     let h1 = document.createElement('h1')
@@ -85,7 +85,7 @@
 //     p.innerHTML = `${value.monthDuration}`
 //
 //     mainDiv.append(itemDiv)
-//     itemDiv.append(h1,p)
+//     itemDiv.append(h1, p)
 //
 //
 //     document.body.append(mainDiv)
