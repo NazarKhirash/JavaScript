42df1b6077191d7437d951f1374c8bb5ccc13310
NazarKhirash <hirashnazar@ukt.net>
2022-01-09 17:25:30 +0200

    HW-11 done

---
+++ JS-HW/HW-11/main.js
@@ -0,0 +1,35 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+
+// let form = document.forms.username;
+// let btn = document.getElementById('btn')
+//
+// btn.onclick = function (){
+//         let user = {
+//                 name: form.name.value,
+//                 age: form.age.value
+//         }
+//     localStorage.setItem('user', JSON.stringify(user))
+// }
+//
+//----------------------------------------------------------------------------------------------------------------------
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+// let formCar = document.forms.car
+// let btn = document.getElementById('btn')
+// let arrCar = []
+//
+// btn.onclick = function () {
+//     let carObj = {
+//         model: formCar.model.value,
+//         type: formCar.type.value,
+//         volume: formCar.volume.value
+//     }
+//
+//     arrCar.push(carObj)
+//     localStorage.setItem('keyCar', JSON.stringify(arrCar))
+// }
+//
+
+
